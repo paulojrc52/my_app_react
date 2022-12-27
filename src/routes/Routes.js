81d6bd4bd914a0ab5f90635832c26c03f3719645
Paulo Júnior <paulojrcosta300@gmail.com>
2022-12-27 13:55:18 -0300

    adicionando estrutura inicial

---
+++ src/routes/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Switch, Route} from 'react-router-dom'
+
+import Home from '../pages/Home'
+
+const Routes = () => {
+  return(
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  )
+}
+
+export default Routes
