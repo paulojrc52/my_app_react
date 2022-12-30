f6666d1e066a14cd6520a046c03621da6f07ebbc
Paulo Júnior <paulojrcosta300@gmail.com>
2022-12-30 10:54:17 -0300

    criando rota para página de clientes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Switch, Route} from 'react-router-dom'
+import Customers from '../pages/Customers'
 
 import Home from '../pages/Home'
 
 const Routes = () => {
   return(
     <Switch>
+      <Route path="/customers">
+        <Customers />
+      </Route>
       <Route path="/">
         <Home />
       </Route>
