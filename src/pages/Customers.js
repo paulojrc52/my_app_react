4c1b9ec3bd313b64b76bf88c5718be5464b2131d
Paulo Júnior <paulojrcosta300@gmail.com>
2022-12-30 12:55:46 -0300

    adicionando e personalizando grid

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,34 +1,53 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
+import { Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
+
 import CustomerCard from "../components/CustomerCard"
 
+const useStyles = makeStyles(theme => ({
+  root:{
+    flexGrow: 1,
+  },
+  card: {
+    margin: theme.spacing(4)
+  },
+}))
+
 const Customers = () => {
-    const [customers, setCustomer] = useState([])
+  const classes = useStyles()
 
-    useEffect(() => {
-      axios.get('https://reqres.in/api/users')
-        .then(response => {
-          const { data } = response.data
-          setCustomer(data)
-      })
-    }, [])
+  const [customers, setCustomer] = useState([])
 
-    return(
-        <>
-          <h1>Clientes</h1>
-          {
-            customers.map(customer => (
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users')
+      .then(response => {
+        const { data } = response.data
+        setCustomer(data)
+    })
+  }, [])
+
+  return(
+    <>
+      <h1>Clientes</h1>
+      <Grid container>
+        {
+          customers.map(customer => (
+            <Grid item xs={12} sm={6} md={4}>
               <CustomerCard 
                 name={customer.first_name}
                 lastname={customer.last_name}
                 email={customer.email}
                 avatar={customer.avatar}
+                className={classes.card}
               />
+            </Grid>
             ))
           }
-        </>
-    )
+      </Grid>
+    </>
+  )
 }
 
 export default Customers
