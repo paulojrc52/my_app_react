8bfaee96d860a322e7830ac834f021448d614902
Paulo Júnior <paulojrcosta300@gmail.com>
2023-01-03 12:49:28 -0300

    criando função de click do button

--- src/pages/customers/Register.js
+++ src/pages/customers/Register.js
@@ -1,6 +1,8 @@
-import { Button, TextField } from "@material-ui/core"
-
+import { useState } from "react"
+import axios from "axios"
 import { makeStyles } from "@material-ui/styles"
+
+import { Button, TextField } from "@material-ui/core" 
 
 const useStyles = makeStyles(theme =>({
   wrapper: {
@@ -11,16 +13,93 @@ const useStyles = makeStyles(theme =>({
 const Register = () => {
   const classes = useStyles()
 
+  const [form, setForm] = useState({
+    name: {
+      value: '',
+      error: false,  
+    } ,
+    job: {
+      value: '',
+      error: false,  
+    },
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({
+      ...form,
+      [name]: {
+        value,
+      } ,
+    })
+  }
+
+  const handleRegisterButton = () => {
+    let hasError = false
+    
+    let newFormState ={
+      ...form,
+    }
+
+    if(!form.name.value) {
+      hasError = true
+      newFormState.name ={
+        value:form.name.value,
+        error: true,
+        helperText: 'Digite o campo nome corretamente'
+      }
+    }
+
+    if(!form.job.value) {
+      hasError = true
+      newFormState.job ={
+        value:form.job.value,
+        error: true,
+        helperText: 'Digite o campo cargo corretamente'
+      }
+    }
+
+    if(hasError) {
+      return setForm(newFormState)
+    }
+
+    axios.post('https://reqres.in/api/users', {
+      name: form.name.value,
+      job: form.job.value
+    }).then(response => {
+        console.log('ok', response)
+      })
+  }
+
   return(
       <>
         <div className={classes.wrapper}>
-          <TextField label='Digite o seu nome' />
+          <TextField 
+            error={form.name.error}
+            helperText={form.name.error ? form.name.helperText : ''}
+            label='Digite o seu nome' 
+            name="name" 
+            value={form.name.value} 
+            onChange={handleInputChange} 
+          />
         </div>
         <div className={classes.wrapper}>
-          <TextField label='Digite o seu cargo' />
+          <TextField 
+            error={form.job.error}
+            helperText={form.job.error ? form.job.helperText : ''}
+            label='Digite o seu cargo' 
+            name="job" 
+            value={form.job.value} 
+            onChange={handleInputChange} 
+          />
         </div>
         <div className={classes.wrapper}>
-          <Button variant='contained' color="primary">
+          <Button 
+            variant='contained' 
+            color="primary" 
+            onClick={handleRegisterButton}
+          >
             Cadastrar
           </Button>
         </div>
