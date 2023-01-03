9adba39db420c679e6d74277fcc314c301c448a0
Paulo Júnior <paulojrcosta300@gmail.com>
2023-01-03 10:45:26 -0300

    criando rota para page register

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,14 +3,18 @@ import { Switch, Route} from 'react-router-dom'
 
 import TemplatePage from '../templates/Page'
 
-import Customers from '../pages/Customers'
+import CustomersList from '../pages/customers/List'
+import CustomersRegister from '../pages/customers/Register'
 import Home from '../pages/Home'
 
 const Routes = () => {
   return(
     <Switch>
+        <Route path="/customers/add">
+        <TemplatePage title='Cadastro de Cliente' Component={CustomersRegister} />
+      </Route>
       <Route path="/customers">
-        <TemplatePage title='Lista de Clientes' Component={Customers} />
+        <TemplatePage title='Lista de Clientes' Component={CustomersList} />
       </Route>
       <Route path="/">
         <TemplatePage title='Página Inicial' Component={Home} />
