8b9c3d6fc0d99e0f79fab08bd532092b4c32df34
Paulo Júnior <paulojrcosta300@gmail.com>
2023-01-04 10:27:41 -0300

    criando componente toasty (snackbar)

---
+++ src/components/Toasty.js
@@ -0,0 +1,40 @@
+import { Snackbar } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
+
+const Toasty = ({ open, text, severity, onClose }) => {
+
+  const handleClose = (event, reason) => {
+    if(reason === 'clickkaway') {
+      return
+    }
+
+    onClose()
+  }
+
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      autoHideDuration={3000}
+      open={open}
+      onClose={handleClose}
+    >
+
+      <MuiAlert 
+        elevation={6} 
+        variant='filled' 
+        severity={severity}
+      >
+
+        {text}
+        
+      </MuiAlert>
+
+    </Snackbar>
+    
+  )
+}
+
+export default Toasty
