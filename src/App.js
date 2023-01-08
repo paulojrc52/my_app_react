c83d43024c13a2351b60b8353bf55636e212aa87
Paulo Júnior <paulojrcosta300@gmail.com>
2023-01-08 12:47:41 -0300

    alterando app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, } from 'react-router-dom'
 
 import Routes from './routes/Routes'
-import TemplateDefault from './templates/Default'
 
 const App = () => {
   return (
     <Router>
-      <TemplateDefault>
-        <Routes />
-      </TemplateDefault>
+      <Routes />
     </Router> 
   )
 }
